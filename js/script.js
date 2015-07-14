d057dfad2b431723752471ff27aa3f82a46a2c38
zmateusz <mat309@o2.pl>
2015-07-14 18:42:12 +0200

    add affix + scrollspy nav

---
+++ js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+
+// $(function(){
+// var sideBarNavWidth=$('#leftcolumn').width() - parseInt($('#sidebarnav').css('paddingLeft')) - parseInt($('#sidebarnav').css('paddingRight'));
+// $('#sidebarnav').css('width', sideBarNavWidth);
+// });
+
+});
